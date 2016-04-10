55a62e73ba61329f21c3060eb39df2ed804d06cb
Gabriel Lalonde <gabriellalonde@outlook.com>
2016-04-10 00:03:29 -0400

    Added ReCaptcha

--- www/html/register.php
+++ www/html/register.php
@@ -1,56 +1,74 @@
 <?php
-$conn_string="host=www2.movieexchange.xyz port=5432 dbname=moviedb user=guest password=20160411Due";
-$dbconn=pg_connect($conn_string) or die("Connection to the Server Failed");
-$success=0;
+// grab recaptcha library
+require_once "recaptchalib.php";
 
+// your secret key
+$secret = "6LeH-BwTAAAAAGVX0ct1KEDBd2d8sw4o4tKO4h22";
 
+// empty response
+$response = null;
 
-if(array_key_exists('save',$_POST)){
+// check secret key
+$reCaptcha = new ReCaptcha($secret);
 
-	$userid=$_POST['userid'];
-	$lname=$_POST['lname'];
-	$fname=$_POST['fname'];
-	$email=$_POST['email'];
-	$gender=$_POST['gender'];
-	$agerange=$_POST['age'];
-	$password=$_POST['password'];
-
-
-
-
-	$checkuserid="SELECT * FROM moviedb.USERS WHERE userid=$1";
-	$stmt=pg_prepare($dbconn, "ps", $checkuserid);
-	$result1=pg_execute($dbconn,"ps",array($userid));
-
-
-	if(!$result1){
-		die("Error in SQL: ".pg_last_error());
-	}
-
-	$row_count=pg_num_rows($result1);
-
-	if($row_count>0){
-		$success=1;
-		pg_free_result($result1);
-	}else{
-
-
-	$query="INSERT INTO moviedb.USERS(userid, lname, fname, email, gender, agerange, password) VALUES ('$userid','$lname','$fname', '$email', '$gender', '$agerange', '$password')";
-	$result=pg_query($dbconn, $query);
-
-	if(!$result){
-		die("Error in SQL entry: ".pg_last_error());
-	}
-	$success=2;
-
-
-	pg_free_result($result);
-	}
-	pg_close($dbconn);
-
+// if submitted check response
+if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
 }
 
+if ($response != null && $response->success) {
 
+	$conn_string="host=www2.movieexchange.xyz port=5432 dbname=moviedb user=guest password=20160411Due";
+	$dbconn=pg_connect($conn_string) or die("Connection to the Server Failed");
+	$success=0;
+
+	if(array_key_exists('save',$_POST)){
+
+		$userid=$_POST['userid'];
+		$lname=$_POST['lname'];
+		$fname=$_POST['fname'];
+		$email=$_POST['email'];
+		$gender=$_POST['gender'];
+		$agerange=$_POST['age'];
+		$password=$_POST['password'];
+
+
+
+
+		$checkuserid="SELECT * FROM moviedb.USERS WHERE userid=$1";
+		$stmt=pg_prepare($dbconn, "ps", $checkuserid);
+		$result1=pg_execute($dbconn,"ps",array($userid));
+
+
+		if(!$result1){
+			die("Error in SQL: ".pg_last_error());
+		}
+
+		$row_count=pg_num_rows($result1);
+
+		if($row_count>0){
+			$success=1;
+			pg_free_result($result1);
+		}else{
+
+
+		$query="INSERT INTO moviedb.USERS(userid, lname, fname, email, gender, agerange, password) VALUES ('$userid','$lname','$fname', '$email', '$gender', '$agerange', '$password')";
+		$result=pg_query($dbconn, $query);
+
+		if(!$result){
+			die("Error in SQL entry: ".pg_last_error());
+		}
+		$success=2;
+
+		pg_free_result($result);
+		}
+		pg_close($dbconn);
+
+	}
+}
 ?>
 
 
@@ -60,6 +78,7 @@ if(array_key_exists('save',$_POST)){
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" type="text/css" href="css/stylesheet.php"/>
 <title>Settings</title>
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
 <body>
@@ -72,9 +91,6 @@ if($success==2){
 }else if($success==1){
 echo "<span style=color:#FF0000;text-align:center;>Username already exists, try again</span>";
 }else{}
-
-
-
  ?>
  <br>
 
@@ -113,6 +129,9 @@ echo "<span style=color:#FF0000;text-align:center;>Username already exists, try 
 </p>
 <p><label for="confpassword">Confirm Password (4-20 characters) </label><br>
 <input name="confpassword" type="password" style = "font-size:7pt;" id="confpassword" pattern=".{4,20}" required/>
+</p>
+<p>
+	<div class="g-recaptcha" data-sitekey="6LeH-BwTAAAAAMtdGVSXwak5Kx-KiEqey8gm7pyn"></div>
 </p>
 <p>
 <input type="submit" name="save" value="Register"/>
